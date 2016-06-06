ef24074af09bb9b0e40187bce51d39043ba2e817
Artem Dubrovskikh <artdubrovskikh@gmail.com>
2016-06-06 17:51:16 +0300

    mapgen works, but only with detalization 4. Starting new branch to correct this.

--- RenderFunc.cpp
+++ RenderFunc.cpp
@@ -10,20 +10,21 @@ bool RenderFunc() {
 	hge->Gfx_Clear(0xB0E5FA);
 
 	int det = settings->land_detalization;
+	int* map = heighmap; // for shortness
 
-	/*hgeQuad* q = new hgeQuad;
-	//q->blend = 0;
-	q->tex = 0;*/
-	for (int i = 0; i < 80; i+=4/settings->land_detalization) {
-		
-		hgeSprite s(0, 0, 0, 0, 0);
+	for (int i = 0; i < 80; i += (4 / det))
+	/* i is 'x' position on screen, where we draw next 4v */
+	{
 
-		s.Render4V(i * 10 * 4 / det, 600 - heighmap[i+x] * 10 * 4 / det + y*10 * 4 / det,
-			i * 10 * 4 / det + 10 * 4 / det, 600 - heighmap[i + 1 + x] * 10 * 4 / det + y*10,
-			i * 10 * 4 / det + 10 * 4 / det, 600,
-			i * 10 * 4 / det, 600);
+		static hgeSprite s(0, 0, 0, 0, 0);
+
+		s.Render4V(float(i * 10), float(600 - map[x + i] * 10 + y*10),
+			float(i * 10 + 10), float(600 - map[x + i + 1] * 10 + y*10),
+			float(i * 10 + 10), 600.0f,
+			float(i * 10), 600.0f);
 
 	}
+
 
 	/* 0xFED448 - ground color */
 
